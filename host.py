711ae48184683148a4af0deffce973a859ec8c08
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-07-17 13:36:16 +0400

    Feat: Created Flask server

---
+++ host.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+
+app = Flask(__name__)
+model = joblib.load('svm_model.pkl')
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # Serves the empty HTML file
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    processeddata = [[data['slider1'], data['slider2'], data['slider3'], data['slider4'], data['slider5']]]
+    prediction = model.predict(processeddata)
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
